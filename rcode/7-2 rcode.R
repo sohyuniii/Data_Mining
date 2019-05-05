304d9640449efb903693ea1bc143ebbc87bda533
IMSOHYUN <41772329+sohyuniii@users.noreply.github.com>
2019-05-05 13:27:55 +0900

    Add files via upload

--- rcode/7-2 rcode.R
+++ rcode/7-2 rcode.R
@@ -1,6 +1,7 @@
 ##### Exercise 8.8 ##### 
 library(ISLR)
 set.seed(1)
+names(Carseats)
 train <- sample(1:nrow(Carseats), nrow(Carseats) / 2)
 Carseats.train <- Carseats[train, ]
 Carseats.test <- Carseats[-train, ]
@@ -24,14 +25,95 @@ text(prune.carseats, pretty = 0)
 yhat <- predict(prune.carseats, newdata = Carseats.test)
 mean((yhat - Carseats.test$Sales)^2)
 
+library (randomForest)
 bag.carseats <- randomForest(Sales ~ ., data = Carseats.train, mtry = 10, ntree = 500, importance = TRUE)
 yhat.bag <- predict(bag.carseats, newdata = Carseats.test)
 mean((yhat.bag - Carseats.test$Sales)^2)
 importance(bag.carseats)
+varImpPlot(bag.carseats)
 
 rf.carseats <- randomForest(Sales ~ ., data = Carseats.train, mtry = 3, ntree = 500, importance = TRUE)
 yhat.rf <- predict(rf.carseats, newdata = Carseats.test)
 mean((yhat.rf - Carseats.test$Sales)^2)
 importance(rf.carseats)
+varImpPlot(rf.carseats)
 
 ##### Exercise 8.9 ##### 
+set.seed(1)
+train <- sample(1:nrow(OJ), 800)
+names(OJ)
+OJ.train <- OJ[train, ]
+OJ.test <- OJ[-train, ]
+
+tree.oj <- tree(Purchase ~ ., data = OJ.train)
+summary(tree.oj)
+tree.oj
+plot(tree.oj)
+text(tree.oj, pretty = 0)
+tree.pred <- predict(tree.oj, OJ.test, type = "class")
+table(tree.pred, OJ.test$Purchase)
+
+cv.oj <- cv.tree(tree.oj, FUN = prune.misclass)
+cv.oj
+plot(cv.oj$size, cv.oj$dev, type = "b", xlab = "Tree size", ylab = "Deviance")
+points(2, cv.oj$dev[2], col = "red", cex = 2, pch = 20)
+
+prune.oj <- prune.misclass(tree.oj, best = 2)
+plot(prune.oj)
+text(prune.oj, pretty = 0)
+summary(tree.oj) ; summary(prune.oj)
+
+prune.pred <- predict(prune.oj, OJ.test, type = "class")
+table(prune.pred, OJ.test$Purchase)
+1 - (119 + 81) / 270
+
+##### Exercise 8.10 ##### 
+dim(Hitters)
+Hitters <- na.omit(Hitters) ; dim(Hitters)
+Hitters$Salary <- log(Hitters$Salary)
+train <- 1:200
+Hitters.train <- Hitters[train, ]
+Hitters.test <- Hitters[-train, ]
+
+library(gbm)
+set.seed(1)
+pows <- seq(-10, -0.2, by = 0.1)
+lambdas <- 10^pows
+train.err <- rep(NA, length(lambdas))
+for (i in 1:length(lambdas)) {
+  boost.hitters <- gbm(Salary ~ ., data = Hitters.train, distribution = "gaussian", n.trees = 1000, shrinkage = lambdas[i])
+  pred.train <- predict(boost.hitters, Hitters.train, n.trees = 1000)
+  train.err[i] <- mean((pred.train - Hitters.train$Salary)^2)
+}
+plot(lambdas, train.err, type = "b", xlab = "Shrinkage values", ylab = "Training MSE")
+
+set.seed(1)
+test.err <- rep(NA, length(lambdas))
+for (i in 1:length(lambdas)) {
+  boost.hitters <- gbm(Salary ~ ., data = Hitters.train, distribution = "gaussian", n.trees = 1000, shrinkage = lambdas[i])
+  yhat <- predict(boost.hitters, Hitters.test, n.trees = 1000)
+  test.err[i] <- mean((yhat - Hitters.test$Salary)^2)
+}
+plot(lambdas, test.err, type = "b", xlab = "Shrinkage values", ylab = "Test MSE")
+min(test.err)
+lambdas[which.min(test.err)]
+
+library(glmnet)
+fit1 <- lm(Salary ~ ., data = Hitters.train)
+pred1 <- predict(fit1, Hitters.test)
+mean((pred1 - Hitters.test$Salary)^2)
+
+x <- model.matrix(Salary ~ ., data = Hitters.train)
+x.test <- model.matrix(Salary ~ ., data = Hitters.test)
+y <- Hitters.train$Salary
+fit2 <- glmnet(x, y, alpha = 0)
+pred2 <- predict(fit2, s = 0.01, newx = x.test)
+mean((pred2 - Hitters.test$Salary)^2)
+
+boost.hitters <- gbm(Salary ~ ., data = Hitters.train, distribution = "gaussian", n.trees = 1000, shrinkage = 0.07943282)
+summary(boost.hitters)
+
+set.seed(1)
+bag.hitters <- randomForest(Salary ~ ., data = Hitters.train, mtry = 19, ntree = 500)
+yhat.bag <- predict(bag.hitters, newdata = Hitters.test)
+mean((yhat.bag - Hitters.test$Salary)^2)
